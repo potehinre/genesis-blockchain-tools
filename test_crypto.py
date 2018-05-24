f3bca33a7b13f06358206e3255c98de83b460f7c
blitzstern5 <blitzstern5@gmail.com>
2018-05-24 09:50:06 +0300

    Fix crypto tests

--- test_crypto.py
+++ test_crypto.py
@@ -4,7 +4,9 @@ from genesis_blockchain_tools.crypto import *
 
 
 try:
-    from genesis_blockchain_tools.crypto.backend.fastecdsa import Point, crypto
+    from genesis_blockchain_tools.crypto.backend.fastecdsa import (
+            Point, curve, point_to_hex_str
+    )
     def test_point_to_hex_str():
         x = 0x60b34e6babe8fab2302ff27dd50e4e3e3f215f6e6c3b62c3b07272d2bb5046b
         y = 0x629686c5c6fecfda1fea9944e259f7e7d95569a7a33c80034121edaece4ecf1

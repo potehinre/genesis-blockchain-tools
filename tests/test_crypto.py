2a59519340517628d28b5785fef3d64b89689a59
blitzstern5 <blitzstern5@gmail.com>
2018-06-08 12:44:33 +0300

    Add tests for public key None/RAW/04 formats

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -22,11 +22,30 @@ def test_get_public_key():
     assert crypto.get_public_key("15b17816c315cbc3a214edaad3661018061ef2936e63a0a93bdb7644c131ad2d") == "bdbb17d50e7c0ecc23aebc0cb8e6b744e7ee3a63644f49548f6af2afcc699cd9b1ff50abed237fef39ed11efebf0a74e550c03db5caad51345ba2aeae89d3601"
     assert crypto.get_public_key("2922bee6973370915cc63ab5ab8b7a57e1cab909477d7a030b2e4661e7aa2202") == "229ec0d7b943f5e2c2558237c93d9e1e7d5b5cda84d34cfdb0348ec353f4809f2edb841b4ef8718bcdee012eff817fb2b254c991281ef3d563a17bc1a30e4b7d"
 
+    assert crypto.get_public_key("15b17816c315cbc3a214edaad3661018061ef2936e63a0a93bdb7644c131ad2d", fmt='RAW') == "bdbb17d50e7c0ecc23aebc0cb8e6b744e7ee3a63644f49548f6af2afcc699cd9b1ff50abed237fef39ed11efebf0a74e550c03db5caad51345ba2aeae89d3601"
+    assert crypto.get_public_key("2922bee6973370915cc63ab5ab8b7a57e1cab909477d7a030b2e4661e7aa2202", fmt='RAW') == "229ec0d7b943f5e2c2558237c93d9e1e7d5b5cda84d34cfdb0348ec353f4809f2edb841b4ef8718bcdee012eff817fb2b254c991281ef3d563a17bc1a30e4b7d"
+
+    assert crypto.get_public_key("15b17816c315cbc3a214edaad3661018061ef2936e63a0a93bdb7644c131ad2d", fmt='04') == "04bdbb17d50e7c0ecc23aebc0cb8e6b744e7ee3a63644f49548f6af2afcc699cd9b1ff50abed237fef39ed11efebf0a74e550c03db5caad51345ba2aeae89d3601"
+    assert crypto.get_public_key("2922bee6973370915cc63ab5ab8b7a57e1cab909477d7a030b2e4661e7aa2202", fmt='04') == "04229ec0d7b943f5e2c2558237c93d9e1e7d5b5cda84d34cfdb0348ec353f4809f2edb841b4ef8718bcdee012eff817fb2b254c991281ef3d563a17bc1a30e4b7d"
+
 def test_gen_keypair():
     priv_key, pub_key = crypto.gen_keypair()
     assert priv_key != pub_key
     assert len(pub_key) == 2 * len(priv_key)
     pub_key2 = crypto.get_public_key(priv_key)
+    assert pub_key == pub_key2
+
+    priv_key, pub_key = crypto.gen_keypair(pub_key_fmt='RAW')
+    assert priv_key != pub_key
+    assert len(pub_key) == 2 * len(priv_key)
+    pub_key2 = crypto.get_public_key(priv_key)
+    assert pub_key == pub_key2
+
+    priv_key, pub_key = crypto.gen_keypair(pub_key_fmt='04')
+    assert priv_key != pub_key
+    assert pub_key[0:2] == '04'
+    assert len(pub_key) == 2 + 2 * len(priv_key)
+    pub_key2 = crypto.get_public_key(priv_key, fmt='04')
     assert pub_key == pub_key2
 
 def test_sign():

44872d6ad99d1081326a02ce7f08bc61acd91c1e
blitzstern5 <blitzstern5@gmail.com>
2018-07-19 05:41:26 +0300

    Remove old stuff. Keep fastecdsa sign with the only raw mode for now

--- genesis_blockchain_tools/crypto/backend/fastecdsa.py
+++ genesis_blockchain_tools/crypto/backend/fastecdsa.py
@@ -14,12 +14,10 @@ def point_to_hex_str(key, fmt='RAW'):
     elif fmt == '04':
         return '04' + hex_str
 
-#def gen_private_key(curve=curve.P256):
 def gen_private_key(curve=curve.P256, hashfunc=sha256):
     priv_key = keys.gen_private_key(curve)
     return format(priv_key, 'x')
 
-#def get_public_key(priv_key, curve=curve.P256):
 def get_public_key(priv_key, curve=curve.P256, hashfunc=sha256, fmt='RAW'):
     priv_key = int(priv_key, 16)
     pub_key = keys.get_public_key(priv_key, curve=curve)
@@ -33,7 +31,6 @@ def gen_keypair(curve=curve.P256):
 
 def sign(priv_key, data, hashfunc=sha256, curve=curve.P256, sign_fmt='DER',
          sign_size=32):
-    if sign_fmt == 'DER':
     r, s = ecdsa.sign(data, int(priv_key, 16), hashfunc=hashfunc)
     signature = encode_sig(r, s, fmt=sign_fmt, size=sign_size).hex()
     return signature
